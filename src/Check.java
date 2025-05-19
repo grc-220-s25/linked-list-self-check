d3787b269c6f13ce48aa0394cad0fdda5fb753bc
Auberon López <auberonedu@gmail.com>
2025-05-19 09:32:22 -0700

    Initial Skeleton

---
+++ src/Check.java
@@ -0,0 +1,17 @@
+public class Check {
+    /**
+     * Given the head of a linked list, return whether there are more even or 
+     * odd values in the nodes.
+     * 
+     * If there are more even nodes, return "even"
+     * If there are more odd nodes, return "odd"
+     * If there are an equal of even and odd nodes, return "equal"
+     * If head is null, return "equal"
+     * 
+     * @param head the head of the list
+     * @return "even", "odd", or "equal" depending on whether there are more even or odd values
+     */
+    public static String evenOdd(ListNode head) {
+        return null;
+    }
+}
