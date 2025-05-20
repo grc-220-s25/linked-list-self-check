a0dec661efb0425bc5edfa50d9422cabec68ee1f
Kiyum-06 <kiyum1406@gmail.com>
2025-05-20 15:22:53 -0700

    Worked 12 minutes in this method.

--- src/Check.java
+++ src/Check.java
@@ -1,6 +1,6 @@
 public class Check {
     /**
-     * Given the head of a linked list, return whether there are more even or 
+     * Given the head of a linked list, return whether there are more even or
      * odd values in the nodes.
      * 
      * If there are more even nodes, return "even"
@@ -9,9 +9,33 @@ public class Check {
      * If head is null, return "equal"
      * 
      * @param head the head of the list
-     * @return "even", "odd", or "equal" depending on whether there are more even or odd values
+     * @return "even", "odd", or "equal" depending on whether there are more even or
+     *         odd values
      */
     public static String evenOdd(ListNode head) {
-        return null;
+        if (head == null) {
+            return "equal";
+        }
+
+        ListNode current = head;
+        int odd = 0;
+        int even = 0;
+
+        while (current != null) {
+            if (current.data % 2 != 0) {
+                odd++;
+            } else {
+                even++;
+            }
+            current = current.next;
+        }
+
+        if (odd > even) {
+            return "odd";
+        } else {
+            return "even";
+        }
+
+        // didn't get time to work with if(odd == even)return "equal";
     }
 }
