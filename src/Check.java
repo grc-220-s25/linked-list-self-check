bfde40a79027bdb7e6228c3236b4754ac0400010
Akai <161771111+Akaiokaa@users.noreply.github.com>
2025-05-20 16:54:27 -0700

    Finished evenOdd method

--- src/Check.java
+++ src/Check.java
@@ -12,6 +12,28 @@ public class Check {
      * @return "even", "odd", or "equal" depending on whether there are more even or odd values
      */
     public static String evenOdd(ListNode head) {
-        return null;
+        int odd = 0;
+        int even = 0;
+        ListNode current = head;
+        if(head == null){
+            return "equal";
+        }
+        while (current != null) {
+            if (current.data % 2 != 0) {
+                odd++;
+            } else{
+                even++;
+            }
+            current = current.next;
+        }
+        if (odd > even) {
+            return "odd";
+        }
+        else if (even > odd){
+            return "even";
+        } else {
+            return "equal";
+        }
     }
+    //Time remaining 5:58/12:00
 }
