ee3a014d9f97868d94b439a1a5c5332d1fcf9c02
Iquer_Dough <206500499+DennysSaenz123@users.noreply.github.com>
2025-05-21 09:25:22 -0700

    Completed timed practice -ran out of time

--- src/Check.java
+++ src/Check.java
@@ -12,6 +12,24 @@ public class Check {
      * @return "even", "odd", or "equal" depending on whether there are more even or odd values
      */
     public static String evenOdd(ListNode head) {
-        return null;
+        int even = 0;
+        int odd =0;
+
+        while(head != null){
+            if(head.data%2 != 0){
+                odd++;
+                head = head.next;
+            } else if(head.data % 2 == 0 ){
+                even++;
+                head = head.next;
+            }
+        }
+        if(odd > even){
+            return "odd";
+        } else if (even > odd){
+            return "even";
+        } else{
+            return "equal";
+        }
     }
-}
+ }
